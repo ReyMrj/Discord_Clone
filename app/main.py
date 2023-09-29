ef4cdb1dab9f02a984d8474b20cfe414dacf1b00
ReyMrj <rey.dev.ne@gmail.com>
2023-09-29 23:08:46 +0100

    commit

---
+++ app/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
+
+@app.post("/items/")
+async def create_item(item: dict):
+    # In a real application, you would save the item to the database or perform other operations.
+    return {"message": "Item created successfully", "item": item}
+
